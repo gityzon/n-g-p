754d4dacc86296773c4afc5e609f2f394e52a409
gityzon <109445070+gityzon@users.noreply.github.com>
2022-08-17 15:55:03 +0800

    Update replit.nix

--- replit.nix
+++ replit.nix
@@ -1,9 +1,19 @@
 { pkgs }: {
     deps = [
-        pkgs.unzip
-        pkgs.bashInteractive
+    pkgs.unzip
+    pkgs.bashInteractive
     pkgs.php74
     pkgs.php74Extensions.pdo
-    pkgs.sqlite
+    pkgs.php74Extensions.curl
+    pkgs.php74Extensions.ctype
+    pkgs.php74Extensions.dom
+    pkgs.php74Extensions.json
+    pkgs.php74Extensions.mbstring
+    pkgs.php74Extensions.openssl
+    pkgs.php74Extensions.session 
+    pkgs.php74Extensions.simplexml
+    pkgs.php74Extensions.xml
+    pkgs.php74Extensions.zip
+    pkgs.php74Extensions.yaml
     ];
 }
